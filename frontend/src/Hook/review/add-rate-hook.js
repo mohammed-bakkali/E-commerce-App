f76ea6e6a61e6a582e609e9d0cec7c15a258922d
Mohammed <mohammedbakkali807@gmail.com>
2024-10-17 20:13:34 +0100

    Update

--- frontend/src/Hook/review/add-rate-hook.js
+++ frontend/src/Hook/review/add-rate-hook.js
@@ -43,56 +43,28 @@ const useAddRateHook = (id) => {
     };
 
     const res = await dispatch(createReview(reviewData));
-
     if (loading === false) {
-      if (res) {
-        console.log("test1", res.payload.status);
-        console.log("test2", res.payload);
-        console.log("test3", res.payload.status);
-    
+      if (res) {    
+        if (res.payload.errors && res.payload.errors[0]) {
 
-        if (res.payload.message === "You already added review on this product") {
-          console.log("test2", res.payload.message);
-          toast.error("You already added review on this product");
-    
-        } else if (res.payload.message === "You are not allowed to perform this action") {
-          console.log("test300");
-          toast.error("You are not allowed to perform this action!");
-    
+          if (res.payload.errors[0].msg === "You already added review on this product") {
+            toast.error("You already added review on this product");
+          } 
+          else if (res.payload.errors[0].msg === "You are not allowed to perform this action") {
+            toast.error("You are not allowed to perform this action!");
+          }
+
+        } else if (res.payload.message) {
+          if (res.payload.message === "You are not allowed to perform this action") {
+            toast.error("You are not allowed to perform this action!");
+          }
         }
       }
-      console.log(res);
     }
     
   };
 
-  // const res = useSelector((state) => state.review.createReviews);
 
-  // In your component's useEffect hook
-  // useEffect(() => {
-  //   if (!loading) {
-  //     if (res) {
-  //       console.log("ss", res); // This should now log the payload
-  //       console.log("review:", res.review);
-  //       console.log("status:", res.status);
-  //       if (res.status === 201) {
-  //         toast.success("Review added successfully!");
-  //         setRateText("");
-  //         setRateValue(0);
-  //       }
-  //       if (res.status === 403) {
-  //         toast.success("You are not allowed to do this!");
-  //       }
-  //     } else if (error) {
-  //       console.error("Error creating review:", error);
-  //       // Display the error message to the user
-  //       toast.error(
-  //         error?.errors[0]?.msg ||
-  //           "An error occurred while creating the review."
-  //       );
-  //     }
-  //   }
-  // }, [loading, res, error]);
 
   return {
     rateText,
